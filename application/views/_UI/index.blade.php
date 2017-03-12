0b0155fa62b0865f55aca25a4a6d8b2dc0e9a6fc
Ainul Yakin <ainul086@gmail.com>
2017-03-13 05:34:22 +0700

    UI Galeri

--- application/views/_UI/index.blade.php
+++ application/views/_UI/index.blade.php
@@ -464,6 +464,19 @@
 							</td>
 						</tr>
 						<!-- END DIVIDER -->
+						<!-- DIVIDER -->
+						<tr>
+							<td>{{str_pad($no++,2,'0',STR_PAD_LEFT)}}</td>
+							<td>galeri</td>
+							<td>Halaman galeri</td>
+							<td>galeri</td>
+							<td>Belum</td>
+							<td class="text-center"><i class="fa fa-circle text-green"></i></td>
+							<td width="10%">
+								<a href="{{site_url('ui/galeri')}}" class="btn btn-default btn-xs btn-block"><i class="fa fa-link"></i></a>
+							</td>
+						</tr>
+						<!-- END DIVIDER -->
 					</tbody>
 				</table>
 			</div>
