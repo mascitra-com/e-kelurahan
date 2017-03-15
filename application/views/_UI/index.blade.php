fd69516052ce4ec5e3ddd45a2c1cf7f7d187a411
Rizki Herdatullah <rizkiherda@gmail.com>
2017-03-15 12:46:34 +0700

    Update UI Index

--- application/views/_UI/index.blade.php
+++ application/views/_UI/index.blade.php
@@ -315,7 +315,7 @@
 						<tr>
 							<td>{{str_pad($no++,2,'0',STR_PAD_LEFT)}}</td>
 							<td>SKTM</td>
-							<td>Halaman Manajemen SKTM/td>
+							<td>Halaman Manajemen SKTM</td>
 							<td>surat</td>
 							<td>Sudah</td>
 							<td class="text-center"><i class="fa fa-circle text-green"></i></td>
