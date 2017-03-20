cf0a24b0acd3bbc65ba43ca3af1708ddc65151d5
Rizki Herdatullah <rizkiherda@gmail.com>
2017-03-20 12:46:35 +0700

    Modified View Pengajuan Pindah

--- application/views/kelurahan/pindah_pengajuan_cetak.blade.php
+++ application/views/kelurahan/pindah_pengajuan_cetak.blade.php
@@ -224,7 +224,7 @@
 					</div>
 				</div>
 				<div class="block">
-					<a href="{{ site_url('pindah/cetak/'.$cetak->id) }}" class="btn btn-warning btn-block"><i class="fa fa-print"></i> cetak</a>
+					<a href="{{ site_url('pindah/cetak/'.$cetak->id) }}" class="btn btn-warning btn-block"><i class="fa fa-print"></i> Cetak</a>
 				</div>
 			</div>
 		</div>
