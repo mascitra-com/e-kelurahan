5794ef20e4eca992c18a3bf637d062d293b6a7db
Ainul Yakin <ainul086@gmail.com>
2017-03-21 17:06:16 +0700

    UI ijin usaha

--- application/views/_UI/surat/keterangan_ijin_usaha.blade.php
+++ application/views/_UI/surat/keterangan_ijin_usaha.blade.php
@@ -81,7 +81,7 @@
 					</div>
 					<div class="form-group">
 						<label for="">Alamat Usaha</label>
-						<input type="text" class="form-control" name="alamat" placeholder="alamat usaha"/>
+						<textarea class="form-control" name="alamat" placeholder="alamat usaha" required></textarea>
 					</div>
 					<div class="form-group">
 						<button class="btn btn-primary"><i class="fa fa-save"></i> tambah</button>
